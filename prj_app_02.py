a6c5bff085e34709a2af1f4cbe2c92ce575b1a7d
Elvin Buntoro <98207525+Elbun@users.noreply.github.com>
2026-06-10 14:31:33 +0700

    Update path

--- prj_app_02.py
+++ prj_app_02.py
@@ -14,7 +14,8 @@ print("Start printing")
 st.set_page_config(layout="wide")
 
 #----------------------------- Set folder path
-path = "B. Project/whs_monitor_data/"
+# path = "B. Project/whs_monitor_data/"
+path = ""
 
 #----------------------------- Read data file
 df_master_branch = pd.read_csv(path+"master_warehouse_branches.csv", sep=",")
